e5ed50a26beee9c886f5f1f338d85ee5010b9ca4
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-01-08 05:50:08 +0630

    add: complex jsx

--- expense-tracker/src/components/ExpenseCard.js
+++ expense-tracker/src/components/ExpenseCard.js
@@ -1,5 +1,13 @@
 function ExpenseCard() {
-  return <h2>Hello, React. I'm back!!</h2>;
+  return (
+    <div>
+      <div>January 5th 2022</div>
+      <div>
+        <h2>Birthday</h2>
+        <div>$30</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseCard;
